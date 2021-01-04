f131f09897dceb5f0510b5dbdedf052be77681ab
Berran Remzi <berranremzi@gmail.com>
2021-01-04 16:19:15 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Regex_Viewer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtInput = new System.Windows.Forms.RichTextBox();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.txtResult = new System.Windows.Forms.RichTextBox();
+			this.txtFilter = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// txtInput
+			// 
+			this.txtInput.Location = new System.Drawing.Point(12, 65);
+			this.txtInput.Name = "txtInput";
+			this.txtInput.Size = new System.Drawing.Size(450, 574);
+			this.txtInput.TabIndex = 0;
+			this.txtInput.Text = "";
+			// 
+			// txtPath
+			// 
+			this.txtPath.Location = new System.Drawing.Point(65, 13);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(773, 20);
+			this.txtPath.TabIndex = 1;
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(844, 11);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(75, 23);
+			this.btnOpen.TabIndex = 2;
+			this.btnOpen.Text = "Open";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// txtResult
+			// 
+			this.txtResult.Location = new System.Drawing.Point(468, 65);
+			this.txtResult.Name = "txtResult";
+			this.txtResult.Size = new System.Drawing.Size(450, 574);
+			this.txtResult.TabIndex = 3;
+			this.txtResult.Text = "";
+			// 
+			// txtFilter
+			// 
+			this.txtFilter.Location = new System.Drawing.Point(65, 39);
+			this.txtFilter.Name = "txtFilter";
+			this.txtFilter.Size = new System.Drawing.Size(773, 20);
+			this.txtFilter.TabIndex = 4;
+			this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(47, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Input file";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Regex";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(929, 650);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtFilter);
+			this.Controls.Add(this.txtResult);
+			this.Controls.Add(this.btnOpen);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.txtInput);
+			this.Name = "Form1";
+			this.Text = "Regex Viewer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox txtInput;
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.RichTextBox txtResult;
+		private System.Windows.Forms.TextBox txtFilter;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regex_Viewer
+{
+	public partial class Form1 : Form
+	{
+		string filePath = "";
+		string fileContent = "";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void txtFilter_TextChanged(object sender, EventArgs e)
+		{
+			// Create a pattern for a word that starts with letter "M"  
+			string pattern = txtFilter.Text;
+			// Create a Regex  
+			Regex rg = new Regex(pattern);
+
+			// Get all matches  
+			MatchCollection matchedAuthors = rg.Matches(fileContent);
+			// Print all matched authors  
+
+			txtResult.Text = "";
+			for (int count = 0; count < matchedAuthors.Count; count++)
+			{
+				//Console.WriteLine(matchedAuthors[count].Value);
+				txtResult.Text += matchedAuthors[count].Value + "\n";
+			}
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				//Get the path of specified file
+				filePath = openFileDialog.FileName;
+
+				//Read the contents of the file into a stream
+				var fileStream = openFileDialog.OpenFile();
+
+				using (StreamReader reader = new StreamReader(fileStream))
+				{
+					fileContent = reader.ReadToEnd();
+
+					txtPath.Text = filePath;
+					txtInput.Text = fileContent;
+				}
+			}
+		}
+	}
+}
